09d6a3eff21c82545bad90c5195ccb7d6c461bee
xavier-meyer <xavier.01@orange.fr>
2022-04-01 10:11:17 +0200

    init

---
+++ app.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("button-background");
+
+button.addEventListener("click", function(){
+    button.style.background = "linear-gradient( #A18CD1 0%, #FBC2EB 100%)";
+})
